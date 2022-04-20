40c9b818d5ecc20f170e9f408b5610f450d79ba9
Kenneth <Kennetej@stud.ntnu.no>
2022-04-20 23:14:02 +0200

    Made the stars for review rating interactive and made decrement/increment for the amount button with javascript.

--- javascript/product-info.js
+++ javascript/product-info.js
@@ -31,3 +31,42 @@ function openProductReviews() {
         reviews.classList.remove("containerAnimation");
     }
 }
+
+document.querySelector(".plus-button", "num-to-change").addEventListener("click", incButton)
+document.querySelector(".minus-button").addEventListener("click", decButton)
+
+num = document.querySelector(".num-to-change");
+let a = 1;
+
+function incButton() {
+    a++;
+    num.innerText = a;
+    console.log(a);
+}
+
+function decButton() {
+    if(a > 1) {
+        a--;
+        num.innerText = a;
+        console.log(a);
+    }
+}
+
+const stars = document.querySelectorAll(".star");
+
+stars.forEach((star, i) => {
+    star.onclick = function () {
+        let current_star = i + 1;
+
+        stars.forEach((star, j) => {
+            if (current_star >= j + 1) {
+                star.innerHTML = "&#9733";
+            } else {
+                star.innerHTML = "&#9734";
+            }
+        })
+    }
+})
+
+
+
